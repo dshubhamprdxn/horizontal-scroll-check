047d7f855b7466bede4f89ba88f9c00a728c68b6
dshubhamprdxn <shubhamd@axioned.com>
2021-10-08 16:34:34 +0530

    changed logic

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,46 @@
-gsap.registerPlugin(ScrollTrigger)
 
-let container = document.getElementById("container")
+//HORIZONTAL SCROLL
+gsap.registerPlugin(ScrollTrigger);
+const sections = document.querySelectorAll(".block");
+const scrollContainer = document.querySelector(".panel2");
+const snapBy = 1 / (sections.length - 1);
+const snap = gsap.utils.snap(snapBy);
 
-gsap.to(container, {
-  xPercent: -510,
-  ease: "none",
-  scrollTrigger: {
-    trigger: container,
+const scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none"
+});
+
+const horizontalScroll = ScrollTrigger.create({
+    animation: scrollTween,
+    trigger: scrollContainer,
     pin: true,
     scrub: 1,
-    snap:false,
-    end: () => "+=" + container.offsetWidth
-  }
-})
+    end: () => "+=" + scrollContainer.offsetWidth,
+    snap: {
+        duration: 0.5,
+        delay: 0.0,
+        ease: "none",
+        inertia: false,
+        snapTo: (value, self) => {
+            let snapped = snap(value);
+            if (snapped > value === self.direction > 0) {
+                return snapped;
+            }
+            return snapped + (self.direction > 0 ? snapBy : -snapBy);
+        }
+    }
+});
 
+// total scroll amount divided by the total distance that the sections move gives us the ratio we can apply to the pointer movement so that it fits.
+var dragRatio = scrollContainer.offsetWidth / (window.innerWidth * (sections.length - 1));
+var drag = Draggable.create(".proxy", {
+    trigger: scrollContainer,
+    type: "x",
+    onPress() {
+        this.startScroll = horizontalScroll.scroll();
+    },
+    onDrag() {
+        horizontalScroll.scroll(this.startScroll - (this.x - this.startX) * dragRatio);
+    }
+})[0];
